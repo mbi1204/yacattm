f1df59210251e88957b891bfd388e8650aeaff19
mendoza <mendoza@DESKTOP-LOJP65T>
2017-05-12 13:38:55 -0500

    primera version

---
+++ yacattm/src/main/java/com/sinergitec/yacattm/YacattmApplication.java
@@ -0,0 +1,12 @@
+package com.sinergitec.yacattm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YacattmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YacattmApplication.class, args);
+	}
+}
