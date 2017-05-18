b2b773e0373ce1a6d7574a458c12182b43de2b7d
Alex181945 <alestra55@hotmail.com>
2017-05-18 12:59:52 -0500

    OK

--- yacattm/src/main/java/com/sinergitec/yacattm/controller/seg/LoginCtrl.java
+++ yacattm/src/main/java/com/sinergitec/yacattm/controller/seg/LoginCtrl.java
@@ -37,11 +37,15 @@ public class LoginCtrl {
 	public ModelAndView valida(@ModelAttribute("compania") String cCompania, @ModelAttribute("usuario") String cUsuario,
 			@ModelAttribute("password") String cPassword) {
 		
+		System.out.println(cCompania);
+		System.out.println(cUsuario);
+		System.out.println(cPassword);
+		
 		ModelAndView model = new ModelAndView();		
 		loginRep.getAcceso(cCompania, cUsuario, cPassword);
 		
 		if(loginRep.islResultado()){
-			//model.addObject("error", loginRep.getcMensaje());
+			model.addObject("error", loginRep.getcMensaje());
 			model.setViewName("segloginV");
 			return model;
 		}
