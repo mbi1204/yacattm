68f6e61ba864edf91b178c29d4c3b68bd8129be0
Alex181945 <alestra55@hotmail.com>
2017-05-18 12:41:38 -0500

    OK

--- yacattm/src/main/resources/static/js/segLoginV/segLoginV.js
+++ yacattm/src/main/resources/static/js/segLoginV/segLoginV.js
@@ -6,27 +6,42 @@
 
 function segValidaForm(){
 	
-	if($('#compania').val() == ""){
+	//Recupera los valores de las cajas de texto
+	var cCompania = $('#compania').val();
+	var cUsuario  = $('#usuario').val();
+	var cPassword = $('#password').val();
+	
+	if(cComapania == "" && cUsuario == "" && cPassword == ""){
 		
 		$('#companiaD').addClass('has-danger');
 		$('#compania').addClass('form-control-danger');
-		document.getElementById("errorC").innerHTML = "Ingresa la compañia a la que perteneces";		
-	}
-	
-	if($('#usuario').val() == ""){
+		document.getElementById("errorC").innerHTML = "Ingresa la compañia a la que perteneces";
 		
 		$('#usuarioD').addClass('has-danger');
 		$('#usuario').addClass('form-control-danger');
 		document.getElementById("errorU").innerHTML = "Ingresa tu usuario";
-	}
-	
-	if($('#password').val() == ""){
 		
 		$('#passwordD').addClass('has-danger');
 		$('#password').addClass('form-control-danger');
 		document.getElementById("errorP").innerHTML = "Ingresa tu contraseña";
+	} else if($('#compania').val() == ""){
 		
+		$('#companiaD').addClass('has-danger');
+		$('#compania').addClass('form-control-danger');
+		document.getElementById("errorC").innerHTML = "Ingresa la compañia a la que perteneces";		
+	} else if($('#usuario').val() == ""){
+		
+		$('#usuarioD').addClass('has-danger');
+		$('#usuario').addClass('form-control-danger');
+		document.getElementById("errorU").innerHTML = "Ingresa tu usuario";
+	} else if($('#password').val() == ""){
+		
+		$('#passwordD').addClass('has-danger');
+		$('#password').addClass('form-control-danger');
+		document.getElementById("errorP").innerHTML = "Ingresa tu contraseña";		
 	}
+	
+	$('#formLogin').submit();
 	
 }
 
