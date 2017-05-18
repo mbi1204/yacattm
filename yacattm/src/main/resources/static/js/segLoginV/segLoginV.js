03cb44a94e520b0e38ef6b3fe7dab69aacc561b7
Alex181945 <alestra55@hotmail.com>
2017-05-18 12:46:57 -0500

    ok

--- yacattm/src/main/resources/static/js/segLoginV/segLoginV.js
+++ yacattm/src/main/resources/static/js/segLoginV/segLoginV.js
@@ -24,21 +24,33 @@ function segValidaForm(){
 		$('#passwordD').addClass('has-danger');
 		$('#password').addClass('form-control-danger');
 		document.getElementById("errorP").innerHTML = "Ingresa tu contraseña";
+		
+		return;
+		
 	} else if($('#compania').val() == ""){
 		
 		$('#companiaD').addClass('has-danger');
 		$('#compania').addClass('form-control-danger');
-		document.getElementById("errorC").innerHTML = "Ingresa la compañia a la que perteneces";		
+		document.getElementById("errorC").innerHTML = "Ingresa la compañia a la que perteneces";
+		
+		return;
+		
 	} else if($('#usuario').val() == ""){
 		
 		$('#usuarioD').addClass('has-danger');
 		$('#usuario').addClass('form-control-danger');
 		document.getElementById("errorU").innerHTML = "Ingresa tu usuario";
+		
+		return;
+		
 	} else if($('#password').val() == ""){
 		
 		$('#passwordD').addClass('has-danger');
 		$('#password').addClass('form-control-danger');
-		document.getElementById("errorP").innerHTML = "Ingresa tu contraseña";		
+		document.getElementById("errorP").innerHTML = "Ingresa tu contraseña";
+		
+		return;
+		
 	}
 	
 	$('#formLogin').submit();
