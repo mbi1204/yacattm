81b9e73c591cdc62022d6d0fb437002fd0016b04
Alex Estrada <Alex Estrada@Alex>
2017-06-14 14:47:11 -0500

    ok

---
+++ yacattm/src/main/java/com/sinergitec/yacattm/configuration/AppConfig.java
@@ -0,0 +1,20 @@
+package com.sinergitec.yacattm.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.sinergitec.yacattm.interceptor.RequestInterceptor;
+
+@Configuration
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+	@Autowired
+	RequestInterceptor requestInterceptor;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(requestInterceptor);
+	}
+}
