9e3e456c72873f0556ac1345d9aa79acef5ef26f
Alex Estrada <Alex Estrada@Alex>
2017-06-22 13:57:47 -0500

    correccion redireccion

--- yacattm/src/main/java/com/sinergitec/yacattm/controller/ope/UsosVehCtrl.java
+++ yacattm/src/main/java/com/sinergitec/yacattm/controller/ope/UsosVehCtrl.java
@@ -29,7 +29,7 @@ public class UsosVehCtrl {
 	private static final String VIEW = "/ope/cat/ctUsosVehV";
 	private static final String FORM_ADD = "/ope/cat/ctUsosVehVAddF";
 	private static final String FORM_UPD = "/ope/cat/ctUsosVehVUpdF";
-	private static final String REDIRECT = "redirect:/ope/ctUsosVehV/lista";
+	private static final String REDIRECT = "redirect:/ope/ctUsosVehiculo/lista";
 	
 	private String cError;
 
